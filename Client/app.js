94c60d00fe2148a1ed26d60bd83bb13a6101ce0f
Spencer Cornwall <espene.cornwall@gmail.com>
2016-07-18 16:07:36 +0000

    Did I crash evertything?

---
+++ Client/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var dropbox = require('dropbox');
+
+var app = express();
+
+app.use(express.static('Client'));
+
+app.get('/images', function(req, res) {
+  var dbx = new dropbox({ accessToken: 'kg_gKGD2HoUAAAAAAAAB-RDYetv_dfoZUykrwLAh_Y8ARzWUq7LH4usEUW8WxLf8' });
+  dbx.filesListFolder({path: '/ForSite'})
+    .then(function(response) {
+      console.log(response);
+      res.send(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.send('error');
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log('Server is listening on port: ' + process.env.PORT);
+});
