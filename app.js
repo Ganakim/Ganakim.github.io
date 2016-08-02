e1952b89faa1022aa479caae10a918726191bcf7
Spencer Cornwall <espene.cornwall@gmail.com>
2016-08-02 15:34:24 +0000

    Gallery and pulling from dropbox finished

--- app.js
+++ app.js
@@ -5,20 +5,70 @@ var app = express();
 
 app.use(express.static('Client'));
 
-app.get('/images', function(req, res) {
-  
-  var dbx = new dropbox({ accessToken: 'kg_gKGD2HoUAAAAAAAAB-RDYetv_dfoZUykrwLAh_Y8ARzWUq7LH4usEUW8WxLf8' });
-  dbx.filesListFolder({path: '/ForSite'})
-    .then(function(response) {
-      console.log(response);
-      res.send(response);
-    })
-    .catch(function(error) {
-      console.log(error);
-      res.send('error');
+app.get('/imagesS', function(req, res) {
+var dbx = new dropbox({ accessToken: 'kg_gKGD2HoUAAAAAAAAB-RDYetv_dfoZUykrwLAh_Y8ARzWUq7LH4usEUW8WxLf8' });
+dbx.filesListFolder({path: '/Sketches'})
+  .then(function(response) {
+    var entries = response.entries;
+    
+    var sharedLinks = [];
+    var promises = [];
+    for(var i = 0; i < entries.length; i++) {
+      var currentPath = entries[i].path_lower;
+      promises.push(dbx.sharingCreateSharedLinkWithSettings({
+        path: currentPath
+      }).then(function(resp) {
+        var url = resp.url;
+        
+        url = url.replace("www.dropbox", "dl.dropboxusercontent");
+        
+        sharedLinks.push(url);
+      }));
+    }
+    Promise.all(promises).then(function() {
+      //console.log(sharedLinks.length);
+      res.send(sharedLinks);
     });
+    //res.send(sharedLinks);
+  })
+  .catch(function(error) {
+    console.log(error);
+    res.send('error');
+  });
+  
 });
-
+app.get('/imagesB', function(req, res) {
+var dbx = new dropbox({ accessToken: 'kg_gKGD2HoUAAAAAAAAB-RDYetv_dfoZUykrwLAh_Y8ARzWUq7LH4usEUW8WxLf8' });
+dbx.filesListFolder({path: '/Blender'})
+  .then(function(response) {
+    var entries = response.entries;
+    
+    var sharedLinks = [];
+    var promises = [];
+    for(var i = 0; i < entries.length; i++) {
+      var currentPath = entries[i].path_lower;
+      promises.push(dbx.sharingCreateSharedLinkWithSettings({
+        path: currentPath
+      }).then(function(resp) {
+        var url = resp.url;
+        
+        url = url.replace("www.dropbox", "dl.dropboxusercontent");
+        
+        sharedLinks.push(url);
+      }));
+    }
+    Promise.all(promises).then(function() {
+      //console.log(sharedLinks.length);
+      res.send(sharedLinks);
+    });
+    //res.send(sharedLinks);
+  })
+  .catch(function(error) {
+    console.log(error);
+    res.send('error');
+  });
+  
+});
 app.listen(process.env.PORT, process.env.IP, function() {
   console.log('Server is listening on port: ' + process.env.PORT);
 });
